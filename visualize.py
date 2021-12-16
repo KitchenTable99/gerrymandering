eec76a2cc8e14c6043687fd98b1595cd8b5d2141
KitchenTable99 <ccbitt23@colby.edu>
2021-12-16 16:13:05 -0500

    allowed for multiple pixel advancements per render

--- visualize.py
+++ visualize.py
@@ -13,35 +13,48 @@ class PixelMapAnimator:
     map: PixelMap
     fig: plt.Figure
     ax: plt.Axes
+    frame_gap: int
 
     def __call__(self, i: int) -> None:
         if i == 0:
             self.map.map.plot(ax=self.ax, column='class')
-            return
+            return self.ax.get_children()
 
-        first, second = self.map.pick_swap_pair()
-        self.map.swap_pixels(first, second)
+        for _ in range(self.frame_gap):
+            first, second = self.map.pick_swap_pair()
+            self.map.swap_pixels(first, second)
         self.map.map.plot(ax=self.ax, column='class')
-        return
+        return self.ax.get_children()
 
 
 def time():
     with open('pix.pickle', 'rb') as fp:
         pixel_map: PixelMap = pickle.load(fp)
     fig, ax = plt.subplots(1)
-    animator = PixelMapAnimator(pixel_map, fig, ax)
+    animator = PixelMapAnimator(pixel_map, fig, ax, 1)
     import timeit
 
-    print(timeit.timeit(lambda: animator(1), number=100))
+    # print(timeit.timeit(lambda: animator(1), number=10))
+    # sys.exit()
+    import cProfile
+    import pstats
+
+    with cProfile.Profile() as pr:
+        animator(1)
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.dump_stats(filename='profile.prof')
 
 
 def save():
     with open('pix.pickle', 'rb') as fp:
         pixel_map: PixelMap = pickle.load(fp)
 
+    pixel_map.weights = np.array([10, 20, 0])
     fig, ax = plt.subplots(1)
-    animator = PixelMapAnimator(pixel_map, fig, ax)
-    anim = FuncAnimation(fig, animator, frames=180, interval=500)
+    animator = PixelMapAnimator(pixel_map, fig, ax, 10)
+    anim = FuncAnimation(fig, animator, frames=600, interval=500)
     anim.save('testing.gif', writer='imagemagick', fps=60)
 
 
@@ -50,8 +63,8 @@ def main():
         pixel_map: PixelMap = pickle.load(fp)
 
     fig, ax = plt.subplots(1)
-    animator = PixelMapAnimator(pixel_map, fig, ax)
-    anim = FuncAnimation(fig, animator, frames=180, interval=500)
+    animator = PixelMapAnimator(pixel_map, fig, ax, 1000)
+    anim = FuncAnimation(fig, animator, frames=100, interval=1000)
     plt.show()
 
 
