4bab92083b5f3d6e395fb9b0cc593f6a1d2c64b4
KitchenTable99 <ccbitt23@colby.edu>
2022-01-18 15:51:29 -0500

    ensured resolution numbers get reached

--- pixel_map.py
+++ pixel_map.py
@@ -1,19 +1,38 @@
 # this file contains code needed to create a pixel map GeoDataFrame
 from math import ceil
-from typing import List
+from typing import List, Tuple
+from dataclasses import dataclass
 
+import matplotlib.pyplot as plt
 import geopandas as gpd
 import numpy as np
 import pandas as pd
 from shapely.geometry import Polygon
 
 
-def weighted_intersection(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame, attr_str: str) -> gpd.GeoDataFrame:
+class print_colors:
+    RED = '\033[95m'
+    GREEN = '\033[92m'
+    BOLD = '\033[1m'
+    NORMAL = '\033[0m'
+
+
+@dataclass
+class Bounds:
+    x_min: int
+    y_min: int
+    x_max: int
+    y_max: int
+
+
+def weighted_intersection(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame, attr_str: str, drop_na: bool = False) \
+        -> gpd.GeoDataFrame:
     """This function takes two GeoDataFrames with disjoint geometry and puts some target attribute from the second into
         the first one. This function will overlay the two dataframes and if poly1 from gdf1 intersects poly2 from gdf2
         and the area of the intersectional polygon is 50% of gdf2, 50% of the target attribute from gdf2 will be assigned
         to poly1 in gdf1.
 
+        :param drop_na: should this algorithm drop all np.nan rows
         :param gdf1. The dataframe to gain the target attribute
         :param gdf2. The dataframe from which to take the target attribute
         :param attr_str. The attribute to take. Must be a column name in gdf2.
@@ -39,6 +58,7 @@ def weighted_intersection(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame, attr_s
     inter['gdf2_area'] = inter.apply(lambda r: gdf2.at[r['gdf2_num'], 'area'], axis=1)
     # this gets the area of the gdf2 polygon that the smaller inter polygon came from
     inter['prop ' + attr_str] = (inter['inter_area'] / inter['gdf2_area']) * inter[attr_str]
+    inter.to_pickle('inter.pickle')
     inter = inter[['square_num', 'prop ' + attr_str]]
 
     # group all the smaller inter polygons by square and put them back in the original GeoDataFrame via sum aggregation
@@ -49,10 +69,97 @@ def weighted_intersection(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame, attr_s
         attr = float(attr)
         gdf1.at[square, 'attr_str'] = attr
 
+    if drop_na:
+        gdf1 = gdf1.dropna()
+
     return gdf1.rename(columns={'attr_str': attr_str})
 
 
-def make_pixel_map(voting_map: gpd.GeoDataFrame, population_map: gpd.GeoDataFrame, resolution: float,
+def determine_true_square_len(bounds: Bounds, resolution: int, map_: gpd.GeoDataFrame,
+                              tolerance: int = 250, verbose: bool = False) -> float:
+    if verbose:
+        print(f'Trying {resolution = }...')
+    test_map = get_square_data_frame(bounds, resolution)
+    search_column = 'red_votes' if 'red_votes' in map_.columns else 'population'
+    inter = weighted_intersection(test_map, map_, search_column, drop_na=True)
+    actual_num = len(inter)
+
+    if (resolution - tolerance) <= actual_num <= (resolution + tolerance):
+        if verbose:
+            print(f'Succeeded using {resolution}')
+        return get_square_len(bounds, resolution)
+    elif actual_num < (resolution - tolerance):
+        lower = resolution
+        diff = resolution - actual_num
+        upper = resolution + (10 * diff)
+    else:
+        upper = resolution
+        lower = 0
+
+    while lower < upper:
+        target = (upper + lower) // 2
+        if verbose:
+            print(f'Failed by making {actual_num} pixels. Trying {target}...')
+
+        test_map = get_square_data_frame(bounds, target)
+        inter = weighted_intersection(test_map, map_, search_column, drop_na=True)
+        actual_num = len(inter)
+
+        if (resolution - tolerance) <= actual_num <= (resolution + tolerance):
+            if verbose:
+                print(f'Succeeded using {target}')
+            return get_square_len(bounds, target)
+        elif actual_num < (resolution - tolerance):
+            lower = target + 1
+        else:
+            upper = target
+
+    raise Exception('The upper bound was not placed high enough to begin with.')
+
+
+def get_square_data_frame(bounds: Bounds, resolution: int) -> gpd.GeoDataFrame:
+    # start with a square length
+    square_len = get_square_len(bounds, resolution)
+
+    # figure out how many squares are needed in each dimension
+    num_x_squares = ceil((bounds.x_max - bounds.x_min) / square_len)
+    num_y_squares = ceil((bounds.y_max - bounds.y_min) / square_len)
+
+    squares = make_squares(bounds, num_x_squares, num_y_squares, square_len)
+
+    return gpd.GeoDataFrame(geometry=squares, crs=4269)
+
+
+def get_square_len(bounds: Bounds, resolution: int) -> float:
+    x_len = bounds.x_max - bounds.x_min
+    y_len = bounds.y_max - bounds.y_min
+    area_per_square = (x_len * y_len) / resolution
+
+    return area_per_square ** .5
+
+
+def make_squares(bounds: Bounds, num_x_squares: int, num_y_squares: int, square_len: float) -> List[Polygon]:
+    # create the squares to cover the map
+    squares = []
+    for i in range(num_y_squares):
+        for j in range(num_x_squares):
+            x_start = j * square_len + bounds.x_min
+            x_end = (j + 1) * square_len + bounds.x_min
+            y_start = i * square_len + bounds.y_min
+            y_end = (i + 1) * square_len + bounds.y_min
+
+            square = Polygon([
+                (x_start, y_start),
+                (x_end, y_start),
+                (x_end, y_end),
+                (x_start, y_end)
+            ])
+            squares.append(square)
+
+    return squares
+
+
+def make_pixel_map(voting_map: gpd.GeoDataFrame, population_map: gpd.GeoDataFrame, resolution: int,
                    verbose: bool = False) -> gpd.GeoDataFrame:
     # make sure both GDFs are projected into the same crs
     if verbose:
@@ -74,12 +181,14 @@ def make_pixel_map(voting_map: gpd.GeoDataFrame, population_map: gpd.GeoDataFram
     y_min = min(pop_min[1], vote_min[1])
     x_max = max(pop_max[2], vote_max[2])
     y_max = max(pop_max[3], vote_max[3])
+    bounds = Bounds(x_min, y_min, x_max, y_max)
 
     # determine size of squares
+    if verbose:
+        print('Determining square size...')
+    square_len = determine_true_square_len(bounds, resolution, voting_map, verbose=verbose)
     x_len = x_max - x_min
     y_len = y_max - y_min
-    max_len = max(x_len, y_len)
-    square_len = (max_len * resolution) / 100
 
     # figure out how many squares are needed in each dimension
     num_x_squares = ceil(x_len / square_len)
@@ -87,22 +196,7 @@ def make_pixel_map(voting_map: gpd.GeoDataFrame, population_map: gpd.GeoDataFram
 
     if verbose:
         print('Creating squares...')
-    # create the squares to cover the map
-    squares = []
-    for i in range(num_y_squares):
-        for j in range(num_x_squares):
-            x_start = j * square_len + x_min
-            x_end = (j + 1) * square_len + x_min
-            y_start = i * square_len + y_min
-            y_end = (i + 1) * square_len + y_min
-
-            square = Polygon([
-                (x_start, y_start),
-                (x_end, y_start),
-                (x_end, y_end),
-                (x_start, y_end)
-            ])
-            squares.append(square)
+    squares = make_squares(bounds, num_x_squares, num_y_squares, square_len)
 
     # create squares on a map
     pixel_map = gpd.GeoDataFrame(geometry=squares, crs=4269)
@@ -168,22 +262,25 @@ def main():
 
     nc_pop = gpd.read_file('./nc_data/population.geojson')
 
-    nc_map = make_pixel_map(nc_votes, nc_pop, .1, verbose=True)
+    nc_map = make_pixel_map(nc_votes, nc_pop, 53_900, verbose=True)
+    print(f'Kept {len(nc_map)} squares...')
     nc_map = add_numpy_geometry(nc_map)
     nc_map.to_pickle('full_map.pickle')
 
 
 def test():
+    # TODO: add loss of population, votes, and places with zero people
     nc_votes = gpd.read_file('./nc_data/voters_shapefile/NC_G18.shp')
     nc_votes = nc_votes[['G18DStSEN', 'G18RStSEN', 'geometry']].rename(
         columns={'G18DStSEN': 'blue_votes', 'G18RStSEN': 'red_votes'})
 
-    nc_pop = gpd.read_file('./nc_data/population.geojson')
+    nc_pop = gpd.read_file('north_carolina_population.geojson')
 
-    nc_map = make_pixel_map(nc_votes, nc_pop, 1, verbose=True)
+    nc_map = make_pixel_map(nc_votes, nc_pop, 3000, verbose=True)
     nc_map = add_numpy_geometry(nc_map)
     nc_map.to_pickle('test_map.pickle')
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    test()
